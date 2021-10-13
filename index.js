5cc451cc307d08eef5fdf23f16cdae3668789052
kirmann <k.dkle@hotmail.fr>
2021-10-13 15:03:04 +0200

    intégration dynamique de la page

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-import  recipes  from './js/recipes.js'
-import Header from './js/header.js'
-import Search from './js/search.js'
-const ingredients = []
-recipes.forEach(element => {
-  element.ingredients.forEach(el=> ingredients.push(el.ingredient.toLowerCase()))
+import recipes from "./js/recipes.js";
+import Header from "./js/header.js";
+import Search from "./js/search.js";
+import Keywords from "./js/keyWords.js";
+import SearchBtns from "./js/searchBtns.js";
+const ingredients = [];
+recipes.forEach((element) => {
+  element.ingredients.forEach((el) =>
+    ingredients.push(el.ingredient.toLowerCase())
+  );
 });
 console.log(ingredients);
-let setIngredients = new Set(ingredients)
-setIngredients.forEach(element => {
+let setIngredients = new Set(ingredients);
+setIngredients.forEach((element) => {
   console.log(element);
 });
+
 // génération des éléments DOM
-const body = document.body
-const main = document.createElement('main')
-body.appendChild(main)
-const keyword = document.createElement('div')
-keyword.classList.add('keyword')
+const body = document.body;
+const main = document.createElement("main");
+body.appendChild(main);
+
 //génération du header
-const logo = '../img/logo.png'
-const header = new Header(logo).render()
-main.innerHTML += header
-const loupe ='../img/loupe.svg'
-const search = new Search(loupe).render()
-main.innerHTML += search
-main.appendChild(keyword)
+const logo = "../img/logo.png";
+const header = new Header(logo).render();
+main.innerHTML += header;
 
+//génération de la barre de recherche
+const loupe = "../img/loupe.svg";
+const search = new Search(loupe).render();
+main.innerHTML += search;
 
+//génération des mots clé
+const keyword = document.createElement("div");
+keyword.classList.add("keyword");
+
+main.appendChild(keyword);
+const userChoice = new Keywords("lait", "bleu").render();
+keyword.innerHTML += userChoice;
+const modif = document.querySelector(".userChoice");
+console.log(modif);
+modif.style.background = "green";
+
+//génération des boutons de choix de recherche
+const btns = new SearchBtns('patate','four','spatule').render()
+main.innerHTML+=btns
+console.log(typeof btns);
+
+//génération du conteneur des recettes
+const container = document.createElement('div')
+container.classList.add('container')
+main.appendChild(container)
 
--- js/keyWords.js
+++ js/keyWords.js
@@ -1,26 +1,22 @@
+/**
+ * @param {string} value valeur de l'élément sélectionné
+ * @param {string} color couleur du type d'élémént sélectionné
+ */
 export default class Keywords{
   constructor(value,color){
     this.value=value
     this.color=color
   }
-
+  render(){
+    const userChoice=
+    `
+    <div class="userChoice ${this.color}">
+      <p>${this.value}</p>
+      <img class="croix" src="img/croix.png" alt="" />
+    </div>
+    `
+    return userChoice
+  }
 }
 
-<div class="keyWord">
-        <div class="userChoice">
-          <p>Coco</p>
-          <img class="croix" src="img/croix.png" alt="" />
-        </div>
-        <div class="userChoice">
-          <p>Banane</p>
-          <img class="croix" src="img/croix.png" alt="" />
-        </div>
-        <div class="userChoice">
-          <p>Coco</p>
-          <img class="croix" src="img/croix.png" alt="" />
-        </div>
-        <div class="userChoice">
-          <p>Banane</p>
-          <img class="croix" src="img/croix.png" alt="" />
-        </div>
-      </div>
+   
