a7362d476c99a0c5eab2d902da70089040636507
kirmann <k.dkle@hotmail.fr>
2021-10-13 15:34:39 +0200

    ras

--- js/recipe.js
+++ js/recipe.js
@@ -1,3 +1,40 @@
+/**
+ * @param {string} name nom de la recette
+ * @param {number} time durée de la recette
+ * @param {string} ingredient 
+ * @param {string} recipe recette
+ */
 export default class Recipe{
-  constructor()
+  constructor(name,time,ingredient,recipe){
+    this.name=name
+    this.time=time
+    this.ingredient=ingredient
+    this.recipe=recipe
+  }
+  render(){
+    const card = 
+    `
+    <div class="card">
+      <div class="img"></div>
+      <div class="description">
+        <div class="nameAndTime">
+          <p class="name">${this.name}</p>
+          <div class="time">
+            <img class="clock" src="img/clock.png" alt="horloge" />
+            <p class="time">${this.time}</p>
+          </div>
+        </div>
+        <div class="ingredientsAndRecipe">
+          <ul class="ingredients">
+            ${this.ingredient}
+          </ul>
+          <p class="recipe">${recipe}</p>
+        </div>
+      </div>
+    </div>
+    
+    `
+    return card
+
+  }
 }
