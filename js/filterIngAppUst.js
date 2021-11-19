1898818badc9041fd12a9c7ef02ea9b4eda2b7ed
kirmann <k.dkle@hotmail.fr>
2021-11-19 22:24:14 +0100

    réaffichage des recettes lorsque plus de tags sélectionnés

--- js/filterIngAppUst.js
+++ js/filterIngAppUst.js
@@ -50,7 +50,7 @@ export const filterIngAppUst = (arrayOfRecipes) =>{
 		filterByTag(e,arrayOfRecipesToFilter)
 		const closeBtns = document.querySelectorAll('.croix')
 		closeBtns.forEach(el => el.addEventListener('click', (e)=> {
-			closeKeyword(e,arrayOfRecipesToFilter)
+			closeKeyword(e)
 			
 		}))
 		
@@ -61,14 +61,15 @@ export const filterIngAppUst = (arrayOfRecipes) =>{
 
 
 let arrayOfTags=[]
+let arrayOfTagsLength = 0
+
 let filteredArrayOfRecipesBySearchbar
 let globalArrayOfRecipes = []
 function filterByTag(e,arrayOfRecipes,valueOfTag){
-	console.log(arrayOfRecipes)
-	console.log(filteredArrayOfRecipesBySearchbar)
 	let value = e.target.innerHTML.toLowerCase() // récupére le contenu textuel du tag 
 	globalArrayOfRecipes = arrayOfRecipes
 	if(arrayOfTags.includes(value)){
+		console.log('deja dans le tableau')
 		return
 		// let index = arrayOfTags.indexOf(value)
 		// arrayOfTags.splice(index,1)
@@ -77,70 +78,71 @@ function filterByTag(e,arrayOfRecipes,valueOfTag){
 
 	else{
 		arrayOfTags.push(value)
+		arrayOfTagsLength++
 	}	
 	console.log(arrayOfTags)
-	// filterOnclick(globalArrayOfRecipes,arrayOfTags,e)
+	filterOnclick(globalArrayOfRecipes,arrayOfTags,e)
 	generateKeyword(e)
+	console.log(arrayOfTagsLength)
 
 }
 
 
 
 
-// function filterOnclick(globalArrayOfRecipes,arrayOfTags,e,arrayOfRecipesToRender){		
-	
-// 	let container = document.querySelector('.container')
-// 	arrayOfRecipesToRender=[]
-// 	for(const tag of arrayOfTags){
-// 		for(const recipe of globalArrayOfRecipes){	
-// 			for(const el of recipe.ingredients){
-// 				if(el.ingredient.toLowerCase().match(tag))
-// 					arrayOfRecipesToRender.push(recipe)
+function filterOnclick(globalArrayOfRecipes,arrayOfTags,e,arrayOfRecipesToRender){	
+	let container = document.querySelector('.container')
+	arrayOfRecipesToRender=[]
+	for(const tag of arrayOfTags){
+		for(const recipe of globalArrayOfRecipes){	
+			for(const el of recipe.ingredients){
+				if(el.ingredient.toLowerCase().match(tag))
+					arrayOfRecipesToRender.push(recipe)
 			
-// 			}	
-// 			if(recipe.appliance.toLowerCase().match(tag)){
-// 				arrayOfRecipesToRender.push(recipe)
-// 			}
-// 			for(const el of recipe.ustensils){
-// 				if(el.toLowerCase().match(tag)){
-// 					arrayOfRecipesToRender.push(recipe)
-// 				}
-// 			}
-// 		}
+			}	
+			if(recipe.appliance.toLowerCase().match(tag)){
+				arrayOfRecipesToRender.push(recipe)
+			}
+			for(const el of recipe.ustensils){
+				if(el.toLowerCase().match(tag)){
+					arrayOfRecipesToRender.push(recipe)
+				}
+			}
+		}
 
-// 	}
-// 	let setArrayOfRecipesToRender = [...new Set(arrayOfRecipesToRender)]
-
-// 	container.innerHTML = ''
-// 	if(setArrayOfRecipesToRender.length>0){
-// 		for(const recipe of setArrayOfRecipesToRender){
-// 			container.innerHTML += new Recipe(recipe).render()
-// 		}	
-// 	}
-// 	else{
-// 		console.log('ok')
-
-// 		for(const recipe of recettesBarreRecherche){
-// 			container.innerHTML += new Recipe(recipe).render()
-// 		}	
-// 	}
+	}
+	let setArrayOfRecipesToRender = [...new Set(arrayOfRecipesToRender)]
+	container.innerHTML = ''
+	if(arrayOfTagsLength>0){
+		for(const recipe of setArrayOfRecipesToRender){
+			container.innerHTML += new Recipe(recipe).render()
+		}	
+	}
 	
-// 	// globalArrayOfRecipes=setArrayOfRecipesToRender
-// 	// filterIngAppUst(globalArrayOfRecipes)
+	
+	// globalArrayOfRecipes=setArrayOfRecipesToRender
+	// filterIngAppUst(globalArrayOfRecipes)
 
 
-// 	generateKeyword(e)	
-
-// }
+}
 
 function closeKeyword(e,valueOfTag,arrayOfRecipes){
+	let container = document.querySelector('.container')
 	valueOfTag = e.target.parentElement.children[0].innerHTML.toLowerCase()	
 	let index = arrayOfTags.indexOf(valueOfTag)
+	e.target.parentElement.remove(e.target)
 	console.log(index)
 	arrayOfTags.splice(index,1)
 	console.log(arrayOfTags)
-	e.target.parentElement.remove(e.target)
-	
+	arrayOfTagsLength--
+	console.log(arrayOfTagsLength)
+	if(arrayOfTagsLength == 0){	
+		container.innerHTML = ''
+		console.log('ok')
+		for(const recipe of recettesBarreRecherche){
+			container.innerHTML += new Recipe(recipe).render()
+		}	
+	}
 	
 	// filterByTag(e,arrayOfRecipes,valueOfTag)
 }
